7df587de01c24a80c0e484486f67838b9833901f
Daniel Antonsen <mail@root4loot.com>
2019-05-13 05:02:54 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+A simple script to grab build logs from Travis CI (travis-ci.org and travis-ci.com)
+
+### Usage
+```
+python travislogs.py <organization>
+```
+
+Build logs are stored in org/organization and com/organization
+
+### Example
+```
+python travis.py spotify
+...
+Travis Endpoint: [1/2] Active Repo: [1/60] Build: [5/100] Job: [1/1]
+```
+
+```
+cat org/spotify/dockerfile-maven/jobs/432353567.txt
+```
+
+Tip: Use [ripgrep](https://github.com/BurntSushi/ripgrep) to speed up the grepping process
+```
+rg -ia 'password=' -j 12 --no-line-number --pretty
+```
+
---
+++ travislogs.py
@@ -0,0 +1,69 @@
+import sys
+import os
+import json
+import requests
+
+# travislogger.py by root4loot (github.com/root4loot/travislogs)
+# Grabs build logs from travis-ci.org and travis-ci.com
+# results are stored to ./org and ./com/
+#
+# Usage: python travislogs.py <organization>
+
+def req(url):
+	r=requests.get(url, headers={'Travis-API-Version': '3'})
+	return r.json()
+
+def main(owner):
+	exts = ['org', 'com']
+
+	for eIndex, ext in enumerate(exts):
+		repos = req("https://api.travis-ci."+ext+"/owner/"+owner+"/repos?limit=0")
+		activeRepos = 0
+
+		try:
+			for repo in repos['repositories']:
+				if repo['active']:
+					activeRepos += 1
+
+			if activeRepos == 0:
+				print("No active repo in endpoint ["+str(eIndex+1)+"/"+str(len(exts))+"] (travis-ci."+ext)+")"
+
+			for rIndex, repo in enumerate(repos['repositories']):
+				repoID = repo['id']
+				slug = repo['slug']
+				isActive = repo['active']				
+
+				if isActive == True:
+					builds = req("https://api.travis-ci."+ext+"/repo/"+str(repoID)+"/builds?limit=0")
+
+
+					buildsC = len(builds['builds'])
+					for bIndex, build in enumerate(builds['builds']):
+						if not os.path.exists(ext+'/'+slug+'/'+'jobs'):
+							os.makedirs(ext+'/'+slug+'/'+'jobs')
+
+
+						jobsC = len(build['jobs'])
+						for jIndex, job in enumerate(build['jobs']):
+							sys.stdout.write("Travis Endpoint: ["+str(eIndex+1)+"/"+str(len(exts))+"] Active Repo: ["+str(rIndex+1)+"/"+str(activeRepos)+"] Build: ["+str(bIndex+1)+"/"+str(buildsC)+"] Job: ["+str(jIndex+1)+"/"+str(jobsC)+"]\r")
+							sys.stdout.flush()
+
+							jobId = job['id']
+							if not os.path.exists(ext+'/'+slug+'/jobs/'+str(jobId)+".txt"):
+								f=open(ext+'/'+slug+'/jobs/'+str(jobId)+".txt","w+")
+								
+								r=requests.get("https://api.travis-ci."+ext+"/job/"+str(jobId)+"/log.txt", 
+									headers={'Travis-API-Version': '3'})
+								f.write(r.content)
+								f.close()
+		except:
+			print("Something went wrong. Make sure the organization name is correct")
+
+if __name__== "__main__":
+	try:
+		owner = sys.argv[1]
+		print("")
+		main(owner)
+	except:
+		print("\n\033[94mtravislogs.py by root4loot (github.com/root4loot/travislogs)\033[0m")
+		print("\033[1musage: python travislogs.py <organization>\033[0m\n")
